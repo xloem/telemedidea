05fa5588c40d16b73ebed67a082d420501ad1221
xloem <0xloem@gmail.com>
2021-06-13 16:52:19 -0400

    calculates mean of a frame

--- draft.cxx
+++ draft.cxx
@@ -1,3 +1,17 @@
+#include <iostream>
+
 #include <opencv2/videoio.hpp>
 
-int main(int argc, char *const *argv) {}
+int main(int argc, char *const *argv)
+{
+  cv::VideoCapture cap("MOV_20210514_1424307.mp4");
+
+  if (!cap.isOpened()) {
+    std::cerr << "Failed to open MOV_20210514_1424307.mp4" << std::endl;
+    return -1;
+  }
+
+  cv::Mat frame;
+  cap >> frame;
+  std::cout << "mean of frame 1: " << cv::mean(frame) << std::endl;
+}
